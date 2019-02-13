696d7877dfda986a37cbf7eebed6776910d044db
Andre Kurait <AndreKurait@gmail.com>
2019-02-13 08:23:28 -0600

    remove key

--- sms.py
+++ sms.py
@@ -1,9 +1,5 @@
 from twilio.rest import Client
 
-# Your Account SID from twilio.com/console
-account_sid = "AC81d02543e6fda0c44a977753c167e811"
-# Your Auth Token from twilio.com/console
-auth_token  = "b3fdab5ec0024c7807e243237f8572a4"
 
 client = Client(account_sid, auth_token)
 
